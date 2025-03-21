a6bb0230db8fff05335e1870d79685b6d5723ace
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-03-21 14:23:04 +0000

    J'ai ajouter la logique l'affichage des button et aussi l'affichage des question avec les reponse

--- script.js
+++ script.js
@@ -16,7 +16,70 @@ var questions = [
     }
   ];
   
+  var questionText = document.getElementById('question-text');
+  var reponse1 = document.getElementById('reponse1');
+  var reponse2 = document.getElementById('reponse2');
+  var reponse3 = document.getElementById('reponse3');
+  var nextBtn = document.getElementById('next-btn');
+  var precedentBtn = document.getElementById('precedent-btn');
+  var scoreContainer = document.getElementById('score-container');
+  var recommencer = document.getElementById('recommencer');
+  var block_rep = document.getElementById('block_rep');
 
+//   console.log(questionText);
+//   console.log(reponse1);
+//   console.log(reponse2);
+//   console.log(questions.length);
+
+
+  var counter = -1;
+
+  
+
+
+  nextBtn.addEventListener('click', function(){
+    if (counter == (questions.length) - 1) {
+        nextBtn.style = "display:none;";
+        precedentBtn.style = "display:hidden;";
+        questionText.style = "display:none;";
+        block_rep.style = "display:none;";
+
+        scoreContainer.style = "display:block; justify-items:center";
+    }else{
+    block_rep.style = "display:block;";
+    counter++;
+    let nextBtn = document.getElementById('next-btn');
+    nextBtn.textContent = 'Next';
+
+    nextBtn.style = 'background-color: oklch(0.558 0.288 302.321);';
+    precedentBtn.style = "display:flex;";
+
+
+    questionText.innerHTML = questions[counter]['question']; 
+    reponse1.innerHTML = questions[counter]['choices'][0]; 
+    reponse2.innerHTML = questions[counter]['choices'][1]; 
+    reponse3.innerHTML = questions[counter]['choices'][2]; 
+    }
+
+    
+
+
+
+    
+  });
+
+  precedentBtn.addEventListener('click', function(){
+    if (counter == 0) {
+     precedentBtn.style = "display:hidden;";
+    }
+    counter--;
+    questionText.innerHTML = questions[counter]['question'];
+  });
+ 
+
+  recommencer.addEventListener('click', function(){
+    location.reload();
+  });
 
 
 //   for (let i = 0; i < questions.length; i++) {
