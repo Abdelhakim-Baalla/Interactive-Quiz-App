a7552ad82592fd2e534e960c5636f4983f773f0c
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-03-21 12:28:17 +0000

    J'ai crée un array qui contient la questio et la reponse et avec la  reponse correct

---
+++ script.js
@@ -0,0 +1,26 @@
+var questions = [
+    {
+      question: "1 + 1",
+      choices: ["1", "8", "2" ],
+      correctAnswerIndex: 2
+    },
+    {
+      question: "5 * 2",
+      choices: ["6", "10", "3"],
+      correctAnswerIndex: 1
+    },
+    {
+      question: "4 * 3",
+      choices: ["1", "12", "7"],
+      correctAnswerIndex: 1
+    }
+  ];
+  
+
+
+
+//   for (let i = 0; i < questions.length; i++) {
+//     console.table(questions[i]['correctAnswerIndex']);
+    
+//   }
+
